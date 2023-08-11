aca4d222f9903e8ba50ea80721d1824f2346eefe
王宇昕 <3275317723@qq.com>
2023-08-11 09:21:37 +0800

    Initial Commit

---
+++ EyeconizeParents/EyeconizeParentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EyeconizeParentsApp.swift
+//  EyeconizeParents
+//
+//  Created by mac on 2023/8/11.
+//
+
+import SwiftUI
+
+@main
+struct EyeconizeParentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
